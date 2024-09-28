495eca11de2d3a1582852cbb88d8c5217703e32f
Ghofrane <ghofranechibeni@gmail.com>
2024-09-28 11:54:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1 class="title1">Welcome to my App </h1>
+    <h2>Ghofrane Chibeni</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.title1{
+  color:chocolate;
+}
+h2{
+  color:rgb(134, 178, 57) ;
+}
+</style>
